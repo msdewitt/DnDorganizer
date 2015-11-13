416b84e0e2aa4b7da9bf1f8a0b1124512454e6fd
msdewitt <msdewitt@bsu.edu>
2015-11-13 13:17:09 -0500

    Add NPC statistics and values in class nPCInterface

--- src/msdewitt/dnDOrganizer/View/CampainInterface.java
+++ src/msdewitt/dnDOrganizer/View/CampainInterface.java
@@ -4,10 +4,15 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import msdewitt.dnDOrganizer.Model.NPCStats;
+import msdewitt.dnDOrganizer.Model.NonPlayerCharacter;
 
 public class CampainInterface extends Application{
 	
 	GridPane primaryPane = new GridPane();
+	NPCStats stats = new NPCStats(11,10,14,13,16,17);
+	NonPlayerCharacter npc = new NonPlayerCharacter("Jerry", 20, "Male", stats);
+	nPCInterface npcView = new nPCInterface(npc);
 
 	public static void main(String[]args){
 		Application.launch(CampainInterface.class, args);
@@ -15,6 +20,7 @@ public class CampainInterface extends Application{
 	
 	public void start(Stage primaryStage){
 		configure(primaryStage);
+		primaryPane.add(npcView.buildInterface(), 0, 1);
 		primaryStage.show();
 	}
 
---
+++ src/msdewitt/dnDOrganizer/View/nPCInterface.java
@@ -0,0 +1,44 @@
+package msdewitt.dnDOrganizer.View;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import msdewitt.dnDOrganizer.Model.NonPlayerCharacter;
+
+public class nPCInterface {
+	
+	private static final double INSETS = 20;
+	NonPlayerCharacter npc;
+
+	public nPCInterface(NonPlayerCharacter npc){
+
+		this.npc = npc;
+	}
+	public GridPane buildInterface(){
+		GridPane grid = new GridPane();
+		grid.add(buildNPC(), 0, 0);
+		grid.setPrefSize(300.0, 300.0);
+		return grid;
+	}
+	private GridPane buildNPC() {
+		GridPane pane = new GridPane();
+		Insets insets = new Insets(INSETS,INSETS,INSETS,INSETS);
+		Label nPCInfo = new Label("Name: " + npc.getName() + "\nAge: "+ //
+		String.valueOf(npc.getAge()) + "\nGender: " + npc.getGender());
+		pane.add(nPCInfo,0,2);
+		buildStats(pane);
+		pane.setPadding(insets);
+		return pane;
+	}
+	private void buildStats(GridPane pane) {
+		Label npcStats = new Label("Str: " + String.valueOf(npc.getStats().getStrength()) + " " + //
+				String.valueOf("Dex: " + npc.getStats().getDexterity()) + " "+ //
+				String.valueOf("Con: " + npc.getStats().getConstitution()) + "\n" +//
+				String.valueOf("Wis: " + npc.getStats().getWisdom())+ " " + //
+				String.valueOf("Int: " + npc.getStats().getIntelligence()) + " " + //
+				String.valueOf("Cha: " + npc.getStats().getCharisma()));
+		pane.add(npcStats, 0, 3);
+
+		
+	}
+}
